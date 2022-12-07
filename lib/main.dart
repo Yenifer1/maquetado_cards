c8463ea1bbed1d4f2e10899f88c573b69706d5b8
yenifer1 <201713631b@utea.edu.pe>
2022-12-07 13:03:36 -0500

    maquetado_cards

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:maquetado_cards/home_page.dart';
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     
+    );
+  }
+}
